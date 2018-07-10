fa5dd941e5980d2bbade36fea82826e23ef4126b
Broderick Hudson <broderick.hudson@mnsu.edu>
2018-07-09 20:19:36 -0500

    Add credentials, skeleton of options file, simple test of Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,15 @@
-# Twitter Bot!
-username = str(input('Enter your username: '))
-password = str(input('Enter your password: '))
+# Make all imports
+import tweepy
+import credentials as crd
+
+# Load all options from options.csv
+file = open('options.csv','r')
+
+# Set up Twitter interaction
+auth = tweepy.OAuthHandler(crd.consumer_key, crd.consumer_secret)
+auth.set_access_token(crd.access_token, crd.access_token_secret)
+api = tweepy.API(auth)
+
+# TEST
+user = api.me()
+print (user.name)
